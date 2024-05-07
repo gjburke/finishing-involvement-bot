3d50c63d2d83b83b14cb6a55b07ce3a887aadc12
gjburke <148822250+gjburke@users.noreply.github.com>
2024-05-06 23:04:23 -0500

    Added POST request for communication

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
 
 app = Flask(__name__)
+
+
+# Routes for loading the template of the main page and for posting queries and getting results
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/submit', methods=["GET", "POST"])
+def submit():
+    if request.method == "POST":
+        query = request.form.get('query')
+        response = make_response(jsonify(query), 200)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    else:
+        response = make_response(jsonify("Bad Request"), 400)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
