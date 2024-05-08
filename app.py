535d103ee2f4033982d9d4e88ced820c17e2a364
gjburke <148822250+gjburke@users.noreply.github.com>
2024-05-07 23:36:57 -0500

    Tried to connect models in backend
    Turns out pythonanywhere doesn't support keras through tensorflow. May have to downgrade and run it through a different module other than tensorflow, which may take some redesigning. We'll see.

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 
-# A very simple Flask Hello World app for you to get started with...
-
+# Model imports
+import joblib
+import numpy as np
+from tensorflow import keras
+from keras.preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Embedding, GlobalAveragePooling1D, Dense, LSTM, Bidirectional
+# Flask imports
 from flask import Flask, render_template, request, jsonify, make_response
 
 app = Flask(__name__)
 
+# Loading the models and tokensizers for intensity and category, along with max_length for padding, along with model labels
+
+classification_labels = ["Arts","Athletics","Business","Culture","Government","Professional","Religion","Service","Social","STEM"]
+intensity_labels = ["1", "2", "3"]
+
+max_length = 35
+intensity_tokenizer = joblib.load('/home/gjburke33/involvement-bot/models/tokenizer_2500.joblib')
+intensity_model = keras.models.load_model("/home/gjburke33/involvement-bot/models/intensity_model.keras")
+category_tokenizer = joblib.load('/home/gjburke33/involvement-bot/models/tokenizer_10000.joblib')
+category_model = keras.models.load_model("/home/gjburke33/involvement-bot/models/category_model.keras")
 
 # Routes for loading the template of the main page and for posting queries and getting results
 
@@ -15,8 +33,23 @@ def index():
 @app.route('/submit', methods=["GET", "POST"])
 def submit():
     if request.method == "POST":
+        # get query
         query = request.form.get('query')
-        response = make_response(jsonify(query), 200)
+        # intensity: tokensize and pad query
+        query_seq = intensity_tokenizer.texts_to_sequences([query])
+        query_padded = pad_sequences(query_seq, maxlen=max_length, padding='post')
+        # intensity: get prediction
+        intensity_prediction = intensity_model.predict(query_padded)
+
+        return make_response(jsonify("Worked up to here"), 200)
+
+        # category: tokensize and pad query
+        query_seq = category_tokenizer.texts_to_sequences([query])
+        query_padded = pad_sequences(query_seq, maxlen=max_length, padding='post')
+        # cateogry: get prediction
+        category_prediction = category_model.predict(query_padded)
+        # Send back data
+        response = make_response(jsonify(classification_labels[np.argmax(category_prediction)] + " " + intensity_labels[np.argmax(intensity_prediction)]), 200)
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
     else:
