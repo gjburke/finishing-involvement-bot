fa771f7bf1b9b6bfdbc9c4d0d1860a5c3052009b
gjburke2 <gjburke2@illinois.edu>
2024-06-13 20:02:07 -0500

    Cleaned structure, added database functionality

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 # Loading the models and tokensizers for intensity and category, along with max_length for padding, along with model labels
 
-classification_labels = ["Arts","Athletics","Business","Culture","Government","Professional","Religion","Service","Social","STEM"]
+classification_labels = ["Arts","Atheltics","Business","Culture","Government","Professional","Religion","Service","Social","STEM"]
 intensity_labels = ["1", "2", "3"]
 
 max_length = 35
@@ -27,10 +27,6 @@ intensity_model = keras.models.load_model(os.path.join(dirname, "models/intensit
 category_tokenizer = joblib.load(os.path.join(dirname, 'models/tokenizer_10000.joblib'))
 category_model = keras.models.load_model(os.path.join(dirname, "models/category_model.keras"))
 
-# Loading a connection to the database for the post request to use
-
-connection = sqlite3.connect("clubs_database.db")
-
 # Routes for loading the template of the main page and for posting queries and getting results
 
 @app.route('/')
@@ -39,27 +35,38 @@ def index():
 
 @app.route('/submit', methods=["GET", "POST"])
 def submit():
+    url = 'http://127.0.0.1:5000/'
     if request.method == "POST":
-        # get query
-        query = request.form.get('query')
-        # intensity: tokensize and pad query
-        query_seq = intensity_tokenizer.texts_to_sequences([query])
-        query_padded = pad_sequences(query_seq, maxlen=max_length, padding='post')
-        # intensity: get prediction
-        intensity_prediction = intensity_model.predict(query_padded)
-        # category: tokensize and pad query
-        query_seq = category_tokenizer.texts_to_sequences([query])
-        query_padded = pad_sequences(query_seq, maxlen=max_length, padding='post')
-        # cateogry: get prediction
-        category_prediction = category_model.predict(query_padded)
-        # get list of category and intensity matches from database
-        cursor = connection.cursor()
-        names, links = cursor.execute(f"SELECT name, link FROM clubs WHERE classification={} AND intensity={}".format(category_prediction, intensity_prediction)).fetchall()
-        package = [names, links]
-        response = make_response(jsonify(package), 200)
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+        try:
+            # get query
+            query = request.form.get('query')
+            # intensity: tokensize and pad query
+            query_seq = intensity_tokenizer.texts_to_sequences([query])
+            query_padded = pad_sequences(query_seq, maxlen=max_length, padding='post')
+            # intensity: get prediction
+            intensity_prediction = intensity_model.predict(query_padded)
+            intensity_prediction = intensity_labels[np.argmax(intensity_prediction)]
+            # category: tokensize and pad query
+            query_seq = category_tokenizer.texts_to_sequences([query])
+            query_padded = pad_sequences(query_seq, maxlen=max_length, padding='post')
+            # cateogry: get prediction
+            category_prediction = category_model.predict(query_padded)
+            category_prediction = classification_labels[np.argmax(category_prediction)]
+            # get list of category and intensity matches from database
+            connection = sqlite3.connect("clubs_database.db", check_same_thread=False)
+            cursor = connection.cursor()
+            sql_query = "SELECT name, link FROM clubs WHERE classification='%s' AND intensity=%s" % (category_prediction, intensity_prediction)
+            print(sql_query)
+            package = cursor.execute(sql_query).fetchall()
+            print(package)
+            response = make_response(jsonify(package), 200)
+            response.headers.add('Access-Control-Allow-Origin', url)
+            return response
+        except Exception as error:
+            response = make_response(jsonify(error), 500)
+            response.headers.add('Access-Control-Allow-Origin', url)
+            return response
     else:
         response = make_response(jsonify("Bad Request"), 400)
-        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Origin', url)
         return response
---
+++ static/scripts.js
@@ -0,0 +1,141 @@
+
+var queryHistory = [];
+var firstInput = true;
+
+// Function to update date and time
+function updateDateTime() {
+    const now = new Date();
+    const formattedDateTime = now.toLocaleDateString('en-US', {
+    weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
+    }) + '  |  ' + (now.toLocaleTimeString('en-US'));
+    document.getElementById('currentDateTime').textContent = formattedDateTime;
+}
+
+// Update date and time on page load and every second
+document.addEventListener('DOMContentLoaded', function(event) {
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+});
+
+// Function to handle form submission
+function submitForm() {
+    var input = document.getElementById('queryInput').value.trim();
+    var output = document.getElementById('queryOutput');
+    //var displayQuery = document.getElementById('inputDisplay');
+    var typedText = document.getElementById('typedText');
+    var exampleQueries = document.querySelector('.description .example-queries');
+
+    if (input === '') {
+        return;
+    }
+
+    //displayQuery.textContent = "Interests: " + input;
+
+    // Dummy code for sending query in, getting matching clubs back
+
+    // Send to backend
+    var headers = {
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:5000/',
+    }
+
+    var jsonData = JSON.stringify(input);
+    // Send data to backend
+    const formData = new FormData();
+    formData.append("query", jsonData);
+
+    //const backendURL = 'https://finishing-involvement-bot.onrender.com/submit';
+    const backendURL = 'http://127.0.0.1:5000/submit';
+
+    fetch(backendURL, {
+        method: 'POST',
+        headers: headers,
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Success:", data)
+        var matchingClubs = data;
+
+        output.innerHTML = ""; // Clear previous results
+
+        matchingClubs.forEach(club => {
+        var queryDiv = document.createElement('div');
+        queryDiv.className = 'query-box';
+
+        var queryText = document.createElement('a');
+        queryText.textContent = club[0];
+        queryText.setAttribute('href', club[1])
+        queryText.setAttribute('target', '_blank')
+        queryText.className = 'query-text';
+        queryDiv.appendChild(queryText);
+
+        output.appendChild(queryDiv);
+        });
+
+        // Add the query to history if it's not already there
+        if (!queryHistory.includes(input)) {
+            queryHistory.push(input);
+            updateHistory();
+        }
+    })
+    .catch(error => console.error("Error:", error));
+
+    // Hide "Enter your interests below" after the first input
+    if (firstInput) {
+        firstInput = false;
+        //displayQuery.classList.add('hide');
+        typedText.classList.remove('hide');
+        exampleQueries.classList.remove('hide');
+    }
+}
+
+// Event listener for submit button click
+document.getElementById('submitBtn').addEventListener('click', submitForm);
+
+// Event listener for pressing Enter key in the input field
+document.getElementById('queryInput').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        submitForm();
+    }
+});
+
+// Function to update the history container with query history
+function updateHistory() {
+    var historyContainer = document.getElementById('history');
+    historyContainer.innerHTML = "<strong>History</strong>";
+
+    queryHistory.forEach(query => {
+    var historyItem = document.createElement('div');
+    historyItem.className = 'history';
+    historyItem.textContent = query;
+    historyItem.addEventListener('click', function() {
+        document.getElementById('queryInput').value = query;
+        submitForm();
+    });
+    historyContainer.appendChild(historyItem);
+    });
+}
+
+// Function to handle clicking on example queries
+function useExampleQuery(query) {
+    document.getElementById('queryInput').value = query;
+    submitForm();
+}
+
+// Function to toggle visibility of history container
+function toggleHistory() {
+    var historyContainer = document.getElementById('historyContainer');
+    if (historyContainer.style.display === 'none') {
+    historyContainer.style.display = 'block';
+    } else {
+    historyContainer.style.display = 'none';
+    }
+}
+
+// Event listener to close history contaisner when clicking outside of it
+document.body.addEventListener('click', function(event) {
+    var historyContainer = document.getElementById('historyContainer');
+    if (!historyContainer.contains(event.target) && event.target.id !== 'historyToggleBtn') {
+    historyContainer.style.display = 'none';
+    }
+});
